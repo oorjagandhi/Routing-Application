9564b5394380f43d423a1d521fe6ec037091b40c
oorjagandhi <oorjagandhi@gmail.com>
2024-05-21 00:43:58 +1200

    Added Javadoc comments throughout the project

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -1,24 +1,47 @@
 package nz.ac.auckland.se281;
 
+/** This class represents a country */
 public class Country {
   private final String name;
   private final String continent;
   private final int taxFees;
 
+  /**
+   * Constructor to create a country object.
+   *
+   * @param name the name of the country
+   * @param continent the continent of the country
+   * @param taxFees the tax fees of the country
+   */
   public Country(String name, String continent, int taxFees) {
     this.name = name;
     this.continent = continent;
     this.taxFees = taxFees;
   }
 
+  /**
+   * Get the name of the country.
+   *
+   * @return the name of the country
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Get the continent of the country.
+   *
+   * @return the continent of the country
+   */
   public String getContinent() {
     return continent;
   }
 
+  /**
+   * Get the tax fees of the country.
+   *
+   * @return the tax fees of the country
+   */
   public int getTaxFees() {
     return taxFees;
   }
--- src/main/java/nz/ac/auckland/se281/CountryNotFoundException.java
+++ src/main/java/nz/ac/auckland/se281/CountryNotFoundException.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se281;
 
+/** This exception is thrown when the country is not found. */
 public class CountryNotFoundException extends Exception {
   public CountryNotFoundException(String countryName) {
     super(countryName);
